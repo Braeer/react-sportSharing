41d7ffa087a7d49fbb66d47e16e222f15d605cfb
Braeer <ggh.eeg.yyj.65@gmail.com>
2024-02-28 18:47:12 +0300

    header_complite_one

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,17 +6,17 @@ const Header = () => {
   return (
     <header>
       <div className={style.header}>
-        <a className='logo' href="">DRIVE</a>
-        <div className='navBar'>
-          <nav className='menu'>
+        <a className={style.logo} href="">Sport Sharing</a>
+        <div className={style.container}>
+          <nav className={style.menu}>
             <ul>
               <li><a href="#">Cars</a></li>
               <li><a href="#">About</a></li>
             </ul>
           </nav>
-          <div className='authorization'>
-            <a className='sing-in' href="#">Sing in</a>
-            <a className='sing-up' href="#">Sing up</a>
+          <div className={style.authorization}>
+            <a className={style.sing_in} href="#">Sing in</a>
+            <a className={style.sing_up} href="#">Sing up</a>
           </div>
         </div>
       </div>
