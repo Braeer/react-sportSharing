f5fd73e884014929d9b8fc9b57be742afd91a654
Braeer <ggh.eeg.yyj.65@gmail.com>
2024-02-29 15:33:57 +0300

    Footer
    Добавлен footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,12 +3,11 @@ import { Link } from 'react-router-dom';
 
 import styles from './styleFooter.module.scss';
 
-
 const Footer = () => {
   return (
-    <div className="footer">
-      <div className="footer__container">
-        <div className='footer__line_1'>
+    <div className={styles.footer}>
+      <div className={styles.footer__container}>
+        <div className={styles.footer__line_1}>
           <ul>
             <li>
               <Link to="/">Home</Link>
@@ -21,14 +20,26 @@ const Footer = () => {
             </li>
           </ul>
         </div>
-        <div className="footer__line_2">
-          <h2 className="footer__title">Контакты</h2>
-          <p className="footer__text">Телефон: +7 (999) 999-99-99</p>
-          <p className="footer__text">E-mail: 0h5V8@example.com</p>
+        <div className={styles.footer__line_2}>
+          <h2>Контакты</h2>
+          <p>
+            Телефон:{' '}
+            <a href="tel:+79999999999" className="footer__text">
+              +7 (999) 999-99-99
+            </a>
+          </p>
+          <p>
+            Email:{' '}
+            <a href="mailto:0h5V8@example.com" type="email" className="footer__text">
+              0h5V8@example.com
+            </a>
+          </p>
         </div>
       </div>
 
-      <p className="footer__text">© 2022 Sport Sharing. Все права защищены.</p>
+      <p className={styles.footer__text}>© 2022 Sport Sharing. Все права защищены.</p>
+      <br />
+      <p className={styles.footer__text}>Программисты на эрдоганах</p>
     </div>
   );
 };
