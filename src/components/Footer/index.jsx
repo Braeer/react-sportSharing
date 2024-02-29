36ba244f051f0bb48ba6749eaf69c0b65e6fa2b3
Braeer <ggh.eeg.yyj.65@gmail.com>
2024-02-29 12:05:59 +0300

    reviews-commit

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import styles from './styleFooter.module.scss';
+
 
 const Footer = () => {
   return (
     <div className="footer">
-      <h3>Footer</h3>
+      <div className="footer__container">
+        <div className='footer__line_1'>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/">Cars</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="footer__line_2">
+          <h2 className="footer__title">Контакты</h2>
+          <p className="footer__text">Телефон: +7 (999) 999-99-99</p>
+          <p className="footer__text">E-mail: 0h5V8@example.com</p>
+        </div>
+      </div>
+
+      <p className="footer__text">© 2022 Sport Sharing. Все права защищены.</p>
     </div>
   );
 };
